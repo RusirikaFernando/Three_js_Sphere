d389c415ff0272c53c49295a71f592d18a918855
RusirikaFernando <thathsaranif7@gmail.com>
2024-06-01 23:39:16 +0530

    Add mouse-driven color animation to the sphere

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64) //3= radius (sizeof ball) , 64= make ball more smooth using more tangels
 const material = new THREE.MeshStandardMaterial({ //define color
   color:'#00ff83',
+  roughness: 0.2,
 })
 
 const mesh = new THREE.Mesh(geometry,material); //mesh used to connect geometry and material
@@ -74,4 +75,30 @@ loop()
 const tl = gsap.timeline({defaults:{duration: 1}})
 tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
 tl.fromTo("nav", {y: "-100%"}, {y:"0%"})
-t1.fromTo(".title", {opacity:0}, {opacity:1})
+tl.fromTo(".title", {opacity:0}, {opacity:1})
+
+
+//mouse animation color
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener('mousemove',(e) => {
+  if(mouseDown){
+    console.log(rgb)
+rgb =
+[
+  Math.round((e.pageX / sizes.width) * 255),
+  Math.round((e.pageY / sizes.height) * 255),
+  150,
+]
+
+let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+gsap.to(mesh.material.color, {
+  r: newColor.r,
+  g: newColor.g,
+  b: newColor.b,
+})
+  }
+})
