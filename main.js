334788bc9b43de3bf8e070dc9563577bccc4e661
RusirikaFernando <thathsaranif7@gmail.com>
2024-06-01 13:33:28 +0530

    Render a sphere in a html canvas

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//scene
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+//Create sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64) //3= radius (sizeof ball) , 64= make ball more smooth using more tangels
+const material = new THREE.MeshStandardMaterial({ //define color
+  color:'#00ff83',
+})
+
+const mesh = new THREE.Mesh(geometry,material); //mesh used to connect geometry and material
+scene.add(mesh)
+
+
+//light
+const light = new THREE.PointLight(0xffffff, 200, 100) //0xffffff, 1, 100
+light.position.set(0, 10, 10)
+scene.add(light)
+
+//camera
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 20
+scene.add(camera)
+
+
+//renderr
+const canvas = document.querySelector(".webg1")
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(800,600)
+renderer.render(scene, camera)
